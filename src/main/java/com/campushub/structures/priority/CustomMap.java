45a1dad268316b4bcfa525c1133840b62b10aa7f
michaelachel43-alt <michaelachel43@gmail.com>
2026-07-31 14:39:36 +0000

    Implement HashTable, CustomMap, and CustomSet

--- src/main/java/com/campushub/structures/priority/CustomMap.java
+++ src/main/java/com/campushub/structures/priority/CustomMap.java
@@ -2,5 +2,32 @@ package com.campushub.structures.priority;
 
 // Owner: Priority Structures
 // TODO: implement CustomMap
-public class CustomMap {
+public class CustomMap<K, V> {
+    private HashTable<K, V> table;
+
+    public CustomMap(){
+        this.table = new HashTable<>();  
+    }
+
+    public void put(key, value){
+        table.put(key, value); 
+    }
+
+    public V get(K key){
+        return table.get(key);
+    }
+
+    public V remove(K key){
+        return table.remove(key);
+    }
+
+    public boolean containsKey(K key){
+        return table.get(key) != null;
+    }
+
+    public int size(){
+        return table.size();
+    }
+
+    
 }
--- src/main/java/com/campushub/structures/priority/CustomSet.java
+++ src/main/java/com/campushub/structures/priority/CustomSet.java
@@ -2,5 +2,31 @@ package com.campushub.structures.priority;
 
 // Owner: Priority Structures
 // TODO: implement CustomSet
-public class CustomSet {
+
+public class CustomSet<E> {
+    
+    private CustomMap<E, Object> map;
+    
+    private static final Object PRESENT = new Object();
+  
+    public CustomSet(){
+        this.map = new CustomMap<>();
+    }   
+
+    public void add(E element) {
+        map.put(element, PRESENT);
+    }
+
+    public void remove(E element) {
+        map.remove(element);
+    }
+
+    public boolean contains(E element) {
+        return map.containsKey(element);
+    }
+
+    public int size() {
+        return map.size();
+    }
 }
+ 
