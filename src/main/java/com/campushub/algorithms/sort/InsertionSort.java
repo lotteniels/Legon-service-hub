34db14ab2fe6938db242b95e941b5c4a2ba7cab2
d-phantom05 <jkeamenumey@st.ug.edu.gh>
2026-08-08 12:26:07 +0000

    Add insertion sort implementation
    Implemented the insertion sort algorithm to sort an array of integers.

--- src/main/java/com/campushub/algorithms/sort/InsertionSort.java
+++ src/main/java/com/campushub/algorithms/sort/InsertionSort.java
@@ -1,6 +1,19 @@
-package com.campushub.algorithms.sort;
+ public int[] insertionSort(int[] arr) {
+        int[] a = arr.clone();
 
-// Owner: Searching, Sorting, and Testing
-// TODO: implement InsertionSort
-public class InsertionSort {
-}
+        // Start from the second element (index 1) since a single element is "sorted"
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];     // the value we're trying to insert correctly
+            int j = i - 1;      // start comparing with the element just before it
+
+            // Shift elements bigger than 'key' one position to the right
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            // Place 'key' into its correct spot
+            a[j + 1] = key;
+        }
+        return a;
+    }
