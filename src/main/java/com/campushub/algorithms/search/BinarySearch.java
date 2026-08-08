908aad51c75ef2c1608cd7c5823f8c801c8800d4
d-phantom05 <jkeamenumey@st.ug.edu.gh>
2026-08-08 12:11:45 +0000

    Add binary search implementation
    Implemented binary search algorithm in Python.

--- src/main/java/com/campushub/algorithms/search/BinarySearch.java
+++ src/main/java/com/campushub/algorithms/search/BinarySearch.java
@@ -1,6 +1,18 @@
-package com.campushub.algorithms.search;
+def binary_search(arr, target):
+    """
+    Search for target in a sorted array using binary search.
+    Returns the index of target if found, else -1.
+    """
+    left, right = 0, len(arr) - 1
 
-// Owner: Searching, Sorting, and Testing
-// TODO: implement BinarySearch
-public class BinarySearch {
-}
+    while left <= right:
+        mid = left + (right - left) // 2  # avoids overflow in other languages
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
